9a4c9a5409ad2a4fb48fcefdff93a281872e7039
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2022-04-22 07:29:45 -0500

    Delete Tesla-Clone site directory

--- Tesla-Clone site/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import carReducer from "../features/car/carSlice.js"
-
-export const store = configureStore({
-  reducer: {
-    car: carReducer
-  },
-});
